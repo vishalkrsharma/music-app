1e8d5ab2a45c505cd644b57f70710bd376795121
vishalkrsharma <vishalkrsharma1234@gmail.com>
2023-06-23 13:19:44 +0530

    fixed

--- src/contexts/FavouritesContext.tsx
+++ src/contexts/FavouritesContext.tsx
@@ -1,15 +1,11 @@
-import { createContext, useEffect, useReducer, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 
 export const FavouritesContext = createContext([]);
 
 export function FavouritesContextProvider({ children }) {
-  const [favourites, setFavourites] = useState([]);
-
-  useEffect(() => {
-    if (localStorage.getItem('favourites')) {
-      setFavourites(JSON.parse(localStorage.getItem('favourites')));
-    }
-  }, [setFavourites]);
+  const [favourites, setFavourites] = useState(() => {
+    return JSON.parse(localStorage.getItem('favourites')) || [];
+  });
 
   useEffect(() => {
     localStorage.setItem('favourites', JSON.stringify(favourites));
